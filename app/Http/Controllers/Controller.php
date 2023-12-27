0c1755c83ecc72ebf92eb973dcecafbbeff9c6b7
SumitAgnihotri <ashishagnihotri715@gmail.com>
2023-12-27 15:12:46 +0530

    working setup

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,7 +11,7 @@ class Controller extends BaseController
     use AuthorizesRequests, ValidatesRequests;
 
     public function data() {
-        return "text";
+        return "working fine";
     }
     
 }
